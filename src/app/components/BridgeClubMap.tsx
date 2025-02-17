d03b0a4b47ddef2a08c15385ff5facf424b3636c
Jacqueline Collier <jacqueline.collier@mnscorp.net>
2025-02-17 22:30:37 +0000

    Use static country data

--- src/app/components/BridgeClubMap.tsx
+++ src/app/components/BridgeClubMap.tsx
@@ -1,46 +1,28 @@
 'use client';
 
-import {
-  APIProvider,
-  Map,
-  MapCameraChangedEvent,
-} from '@vis.gl/react-google-maps';
+import { APIProvider, Map } from '@vis.gl/react-google-maps';
 import Counties from '@/app/components/Counties';
-import { County } from '@/app/model/types';
-// import MapMask from "@/app/components/MapMask";
 import PoiMarkers from '@/app/components/PoiMarkers';
 import { england } from '@/app/model/england';
 import { scotland } from '@/app/model/scotland';
 import { wales } from '@/app/model/wales';
 import { northernIreland } from '@/app/model/northern-ireland';
+import { Country, County } from '@/app/model/types';
+
 const containerStyle = {
   width: '100%',
   height: '100%',
 };
 
 const BridgeClubMap = (props: {
+  selectedCountry: Country | null;
   selectedCounty: County | null;
-  onCountiesLoaded: (counties: County[]) => void;
-  onSelectedCounty: (county: County) => void;
 }) => {
-  const defaultCenter = { lat: 52.3555, lng: -1.1743 }; // UK Center
-  const defaultZoom = 6;
-
   const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API!;
 
   return (
     <APIProvider apiKey={apiKey}>
       <Map
-        defaultZoom={defaultZoom}
-        defaultCenter={defaultCenter}
-        onCameraChanged={(ev: MapCameraChangedEvent) =>
-          console.log(
-            'camera changed:',
-            ev.detail.center,
-            'zoom:',
-            ev.detail.zoom,
-          )
-        }
         mapId='da37f3254c6a6d1c'
         style={containerStyle}
         fullscreenControl={false}
@@ -48,15 +30,12 @@ const BridgeClubMap = (props: {
         mapTypeControl={false}
       >
         <Counties
+          selectedCountry={props.selectedCountry}
           selectedCounty={props.selectedCounty}
-          onCountiesLoaded={props.onCountiesLoaded}
-          onSelectedCounty={props.onSelectedCounty}
         />
         <PoiMarkers
           pois={[...england, ...scotland, ...wales, ...northernIreland]}
         />
-        {/*<MapMask counties={counties} />*/}
-        {/*<MapStyling />*/}
       </Map>
     </APIProvider>
   );
