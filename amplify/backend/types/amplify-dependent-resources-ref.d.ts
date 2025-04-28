39b747384072f14c3a7a9c7bc32218fc9393b1db
Jacqueline Collier <jacqueline.collier@mnscorp.net>
2025-04-28 23:06:09 +0100

    Tidy up amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,20 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
-  api: {
-    ukbridgeclubamplify: {
-      GraphQLAPIEndpointOutput: 'string';
-      GraphQLAPIIdOutput: 'string';
-      GraphQLAPIKeyOutput: 'string';
-    };
-  };
-  auth: {
-    ukbridgeclubamplifyb1a78a80: {
-      AppClientID: 'string';
-      AppClientIDWeb: 'string';
-      IdentityPoolId: 'string';
-      IdentityPoolName: 'string';
-      UserPoolArn: 'string';
-      UserPoolId: 'string';
-      UserPoolName: 'string';
+  function: {
+    ukbridgeclubprocessresults: {
+      Arn: 'string';
+      LambdaExecutionRole: 'string';
+      LambdaExecutionRoleArn: 'string';
+      Name: 'string';
+      Region: 'string';
     };
   };
 };
